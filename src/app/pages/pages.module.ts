706f9c7546e1a4cc85bba625227ff0b176bf4b9c
Agryo Araujo <agryostallion@gmail.com>
2023-10-11 13:39:44 -0300

    Testes dos cards

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { PrimeNgModule } from "../prime-ng/prime-ng.module";
 import { HomeComponent } from './home/home.component';
 import { SobreComponent } from './sobre/sobre.component';
+import { ComponentsModule } from '../components/components.module';
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { SobreComponent } from './sobre/sobre.component';
   imports: [
     FormsModule,
     CommonModule,
-    PrimeNgModule
+    PrimeNgModule,
+    ComponentsModule
   ],
   exports: [
     HomeComponent,
