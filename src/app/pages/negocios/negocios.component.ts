25a40e9f4dcb87cf0d2bf74de77200c00f2c5ba2
Agryo Araujo <agryostallion@gmail.com>
2023-10-18 12:57:46 -0300

    Ajustes em todas as pages

--- src/app/pages/negocios/negocios.component.ts
+++ src/app/pages/negocios/negocios.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NegocioData } from 'src/app/models/negocioData';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-negocios',
   templateUrl: './negocios.component.html',
   styleUrls: ['./negocios.component.css']
 })
-export class NegociosComponent {
+export class NegociosComponent implements OnInit {
+  value: string | undefined;
+  negocioInfo: NegocioData[] = []
 
+  constructor( private apiService: ApiService ) {
+
+  }
+
+  ngOnInit(): void {
+    this.apiService.listarNegocios().subscribe((res: any) => {
+      this.negocioInfo = res.map((item: any) => {
+        return {
+          nome: item.nome,
+          telefone: item.telefones[0]?.numero ?? 'Sem telefone', // Pega o primeiro número da lista ou undefined se a lista não existir
+          descricao: item.descricao
+        };
+      });
+    });
+  }
 }
