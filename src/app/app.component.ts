a2a8a9a72f50e97bca4fee53594b885a913a5b9b
Agryo Araujo <agryostallion@gmail.com>
2023-10-18 00:19:45 -0300

    Ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
-  title = 'Guia-Cruzeta';
+  title = 'Guia Cruzeta | Aqui você encontra.'
 
   constructor() {
   }
